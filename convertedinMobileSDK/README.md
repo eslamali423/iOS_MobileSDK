891d2acea8c58e58edb4c487bfcccc8241505782
eslamali <38584724+eslamali423@users.noreply.github.com>
2023-09-18 19:37:51 +0300

    add: First Commit

---
+++ convertedinMobileSDK/README.md
@@ -0,0 +1,3 @@
+# convertedinMobileSDK
+
+A description of this package.
---
+++ convertedinMobileSDK/Sources/convertedinMobileSDK/convertedinMobileSDK.swift
@@ -0,0 +1,13 @@
+public struct convertedinMobileSDK {
+    public private(set) var text = "Hello, World!!!!!"
+
+    public init() {
+        print("===============")
+        print(text)
+        print(text)
+        print(text)
+        print(text)
+        print(text)
+        print("===============")
+    }
+}
