d212cb62dc119e2bd6ef2c894f22dd83a60ed7a9
eslamali <38584724+eslamali423@users.noreply.github.com>
2023-09-19 14:49:41 +0300

    simplify

---
+++ Sources/convertedinMobileSDK/IdentifyUserModel.swift.swift
@@ -0,0 +1,14 @@
+//
+//  identifyUserModel.swift
+//  
+//
+//  Created by Eslam Ali  on 19/09/2023.
+//
+
+import Foundation
+
+
+struct identifyUserModel: Codable {
+    let cid: String?
+    let  csid: String?
+}
--- Sources/convertedinMobileSDK/convertedinMobileSDK.swift
+++ Sources/convertedinMobileSDK/convertedinMobileSDK.swift
@@ -13,114 +13,11 @@ public struct convertedinMobileSDK {
         self.pixelId = pixelId
         self.storeUrl = storeUrl
         
-        self.identifyUser(email: "eslam.ali4233@gmail.com", countryCode: nil, phone: nil)
+//        self.identifyUser(email: "eslam.ali4233@gmail.com", countryCode: nil, phone: nil)
     }
     
     //MARK:- Functions
-    //    public func identifyUser(email: String?, countryCode: String, phone: String?){
-    //        let url = URL(string: "")
-    //        URLSession.shared.dataTask(with: url!) { data, response, error in
-    //
-    //             guard let data else { return }
-    //             guard let response = response as? HTTPURLResponse, 200 ... 299  ~= response.statusCode else { return }
-    //
-    //             // JSONDecoder() converts data to model of type Array
-    ////             do {
-    ////                 //let products = try JSONDecoder().decode([Products].self, from: data)
-    //////                 completion(.success(products))
-    ////             }
-    ////             catch {
-    ////
-    ////             }
-    //         }.resume()
-    //
-    //    }
-    
-    
-//    public func identifyUser(email: String?, countryCode: String?, phone: String?){
-//
-//        //declare parameter as a dictionary which contains string as key and value combination.
-//        guard let pixelId else {return}
-//        guard let storeUrl else {return}
-//        var parameters = ["csid": "deviceToken" ]
-//        if let email = email {
-//            parameters["email"] = email
-//        }
-//
-//        if let countryCode = countryCode, let phone = phone {
-//            parameters["country_code"] = countryCode
-//            parameters["phone"] = phone
-//        }
-//
-//        //create the url with NSURL
-//        let url = URL(string: "https://test.convertedin.com/api/v1/\(pixelId)/identity")!
-//
-//        //create the session object
-//        let session = URLSession.shared
-//        //now create the Request object using the url object
-//        var request = URLRequest(url: url)
-//        request.httpMethod = "POST" //set http method as POST
-//
-//        do {
-//            request.httpBody = try JSONSerialization.data(withJSONObject: parameters, options: .prettyPrinted) // pass dictionary to data object and set it as request body
-//        } catch let error {
-//            print(error.localizedDescription)
-//
-//        }
-//
-//        //HTTP Headers
-//        request.addValue(storeUrl, forHTTPHeaderField: "Referer")
-//
-//        //create dataTask using the session object to send data to the server
-//        let task = session.dataTask(with: request, completionHandler: { (data: Data?, response: URLResponse?, error: Error?) in
-//
-//            guard error == nil else { return }
-//
-//            guard let data = data else { return }
-//
-//            let outputStr  = String(data: data, encoding: String.Encoding.utf8) as String?
-//            //send this block to required place
-//            print("==========")
-//            print(outputStr)
-//
-//            //            do {
-//            //
-//            //                let userData = try JSONDecoder().decode(identifyUserModel.self, from: data)
-//            //                print("==========")
-//            //                print(userData)
-//            //
-//            //                // Convert to a string and print
-//            //                //                if let JSONString = String(data: jsonData, encoding: String.Encoding.utf8) {
-//            //                //                    print("==========")
-//            //                //                    print(JSONString)
-//            //                //                }
-//            //
-//            //                //              //  let userData = try JSONSerialization.data(withJSONObject: data, options: [])
-//            //                //                let userData = try JSONDecoder().decode(identifyUserModel.self, from: data)
-//            //                //                print("==========")
-//            //                //                print(userData)
-//            //            }
-//            //            catch let error {
-//            //                print(error.localizedDescription)
-//            //            }
-//
-//
-//
-//            //            do {
-//            //                //create json object from data
-//            //                guard let json = try JSONSerialization.jsonObject(with: data, options: .mutableContainers) as? [String: Any] else { return }
-//            //                print(json)
-//            //
-//            //            } catch let error {
-//            //                print(error.localizedDescription)
-//            //            }
-//        })
-//
-//        task.resume()
-//    }
-    
-    
-    func identifyUser(email: String?, countryCode: String?, phone: String?){
+   public func identifyUser(email: String?, countryCode: String?, phone: String?){
         guard let pixelId else {return}
         guard let storeUrl else {return}
         let Url = String(format: "https://test.convertedin.com/api/v1/\(pixelId)/identity")
@@ -146,42 +43,15 @@ public struct convertedinMobileSDK {
         
         let session = URLSession.shared
         session.dataTask(with: request) { (data, response, error) in
-            if let response = response {
-                print(response)
-                
-            }
-            if let data = data {
+            guard error == nil else {return}
+            guard let response = response else {return}
+            guard  let data = data else {return}
                 do {
-                    
-//                    let jsonData = try? JSONSerialization.data(withJSONObject: data, options: [.fragmentsAllowed])
-//                    let decodeed = String(data: jsonData ?? Data(), encoding: .utf8)
-//                    print(decodeed)
-                    
                     let usermodel: identifyUserModel  = try CustomDecoder.decode(data: data)
-//                    let responseData = String(data: data, encoding: String.Encoding.utf8)
-//                    let json = try JSONSerialization.jsonObject(with: data, options: [.allowFragments])
                     print(usermodel)
                 } catch {
                     print(error)
                 }
-            }
         }.resume()
     }
-    
-    
-}
-
-struct identifyUserModel: Codable {
-    let cid: String?
-    let  csid: String?
-}
-
-
-struct CustomDecoder {
-    
-    static func decode<T: Codable>(data: Data) throws -> T {
-        let decoder = JSONDecoder()
-        return try decoder.decode(T.self, from: data)
-    }
-
 }
