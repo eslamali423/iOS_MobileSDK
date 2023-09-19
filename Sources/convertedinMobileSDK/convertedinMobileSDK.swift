60408a09dcbb80892f98009a4d43f02c823f174b
eslamali <38584724+eslamali423@users.noreply.github.com>
2023-09-19 11:35:23 +0300

    add: identifyUserFunction

--- Sources/convertedinMobileSDK/convertedinMobileSDK.swift
+++ Sources/convertedinMobileSDK/convertedinMobileSDK.swift
@@ -1,13 +1,93 @@
-public struct convertedinMobileSDK {
-    public private(set) var text = "Hello, World!!!!!"
+// Convertedin Mobile SDK
 
-    public init() {
-        print("===============")
-        print(text)
-        print(text)
-        print(text)
-        print(text)
-        print(text)
-        print("===============")
+import Foundation
+public struct convertedinMobileSDK {
+ 
+    //MARK:- Variables
+    public private(set) var text = "Hello, World!!!!!"
+    private var pixelId : String?
+    private var storeUrl : String?
+
+    //MARK:- Initlizers
+    public init(pixelId: String, storeUrl: String) {
+        self.pixelId = pixelId
+        self.storeUrl = storeUrl
+        
     }
+    
+    //MARK:- Functions
+//    public func identifyUser(email: String?, countryCode: String, phone: String?){
+//        let url = URL(string: "")
+//        URLSession.shared.dataTask(with: url!) { data, response, error in
+//            
+//             guard let data else { return }
+//             guard let response = response as? HTTPURLResponse, 200 ... 299  ~= response.statusCode else { return }
+//             
+//             // JSONDecoder() converts data to model of type Array
+////             do {
+////                 //let products = try JSONDecoder().decode([Products].self, from: data)
+//////                 completion(.success(products))
+////             }
+////             catch {
+////
+////             }
+//         }.resume()
+//        
+//    }
+    
+    
+    func identifyUser(email: String?, countryCode: String?, phone: String?){
+
+        //declare parameter as a dictionary which contains string as key and value combination.
+        guard let pixelId else {return}
+        guard let storeUrl else {return}
+        var parameters = ["csid": "deviceToken" ]
+        if let email = email {
+            parameters["email"] = email
+        }
+        
+        if let countryCode = countryCode, let phone = phone {
+            parameters["country_code"] = countryCode
+            parameters["phone"] = phone
+        }
+
+        //create the url with NSURL
+        let url = URL(string: "https://test.convertedin.com/api/v1/\(pixelId)/identity")!
+
+        //create the session object
+        let session = URLSession.shared
+        //now create the Request object using the url object
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST" //set http method as POST
+
+        do {
+            request.httpBody = try JSONSerialization.data(withJSONObject: parameters, options: .prettyPrinted) // pass dictionary to data object and set it as request body
+        } catch let error {
+            print(error.localizedDescription)
+            
+        }
+
+        //HTTP Headers
+        request.addValue("Referer", forHTTPHeaderField: storeUrl)
+        
+        //create dataTask using the session object to send data to the server
+        let task = session.dataTask(with: request, completionHandler: { data, response, error in
+
+            guard error == nil else { return }
+
+            guard let data = data else { return }
+
+            do {
+                //create json object from data
+                guard let json = try JSONSerialization.jsonObject(with: data, options: .mutableContainers) as? [String: Any] else { return }
+                print(json)
+                
+            } catch let error {
+                print(error.localizedDescription)
+            }
+        })
+
+        task.resume()
+    }
+
 }
